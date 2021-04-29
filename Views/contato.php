9a244bea09447420cc99f826f990a85f5f1aabd8
Vinícius Vieira <vinivieira0509@gmail.com>
2021-04-29 18:31:18 -0300

    Correção de contatos

--- Views/contato.php
+++ Views/contato.php
@@ -1,23 +1,23 @@
 <?php $titulo = "FERREIRA | CONTATO"; include_once 'layout/header.php'; ?>
 
     <main class="content" style="height: 76vh;">
-        <center><h2 class="logMessages_title mt-5 mb-3">SELECIONE A FORMA DE CONTATO QUE DESEJA UTILIZAR.</h2></center>
+        <center><h2 class="logMessages_title mt-5 mb-3">ESCOLHA A FORMA DE CONTATO CLICANDO NO ÍCONE</h2></center>
         <div class="container ctcards">
             <div class="card col-md-3 cardct">
                 <div class="card-body cdbd bg-primary">
-                    <a href="mailto:financeiro@ferreiraquica.com.BR?subject=Assunto:%20&body=Boa tarde, Ferreira Indústria%0A%0AEscreva sua mensagem...%0A%0AAss: ...%0A%0A"><i class="fas fa-envelope"></i></a>
+                    <a href="mailto:vendas@ferreiraquimica.com.né?subject=Assunto:%20&body=Boa tarde, Ferreira Indústria%0A%0AEscreva sua mensagem...%0A%0AAss: ...%0A%0A"><i class="fas fa-envelope" style="font-size:50px;"></i></a>
                 </div>
                 <div class="mt-3 text-center d-flex justify-content-center">
-                    <small>Redirecionamento para o &nbsp</small><small>E-mail.</small>
+                    <small>Redirecionamento para o &nbsp</small><small>e-mail.</small>
                 </div>
             </div>
         
             <div class="card col-md-3 cardct">
                 <div class="card-body cdbd bg-success">
-                    <a href="https://api.whatsapp.com/send?phone=5541933013750&text=Olá,%20Ferreira%20Indústria!" target="blank"><i class="fas fa-phone-alt"></i></a>
+                    <a href="https://api.whatsapp.com/send?phone=5541999479688&text=Olá,%20Ferreira%20Indústria!" target="blank"><i class="fab fa-whatsapp" style="font-size:50px;"></i></a>
                 </div>
                 <div class="mt-3 text-center d-flex justify-content-center">
-                    <small>Redirecionamento o &nbsp</small><small>WhatsApp Web.</small>
+                    <small>Redirecionamento para o &nbsp</small><small>whatsapp.</small>
                 </div>
             </div>
         </div>
