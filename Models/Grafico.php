c3f0532cc161734bdbfb66d6e1cd4e8f12c00638
vieira-brz <vinivieira0509@gmail.com>
2021-06-02 19:55:43 -0300

    Correção hora maior + soma de gastos e dados

--- Models/Grafico.php
+++ Models/Grafico.php
@@ -3,6 +3,8 @@
 class Grafico
 {
     private $con;
+    public $antigo;
+    public $novo;
 
     public function __construct($conMysql)
     {
@@ -13,16 +15,29 @@ class Grafico
     {
         $query = "SELECT * FROM ".$db.".".$tabelanode." ORDER BY data";
         $dados = $this->con->readQuery($query);   
-
-        foreach ($dados as $key)
+        
+        foreach ($dados as $key => $value)
         {
-            if (strtotime(date('d-m-Y', strtotime($key['data']))) >= strtotime($semana))
+            $this->antigo = date('H', strtotime($value[$key - 1]['data']));
+            $this->novo = date('H', strtotime($value['data']));
+
+            if (strtotime(date('d-m-Y', strtotime($value['data']))) >= strtotime($semana))
             {
-                $retorno['semanal'][strtoupper(utf8_encode(strftime('%a', strtotime($key['data']))))]
-                ['HORARIO'] = date('H', strtotime($key['data']));
+                if ($this->antigo > $this->novo)
+                {
+                    var_dump($this->antigo);
+                    $retorno['semanal'][strtoupper(utf8_encode(strftime('%a', strtotime($value['data']))))]
+                    ['HORARIO'] = $this->antigo;
+                }
+                else
+                {
+                    var_dump($this->novo);
+                    $retorno['semanal'][strtoupper(utf8_encode(strftime('%a', strtotime($value['data']))))]
+                    ['HORARIO'] = $this->novo;
+                }
                 
-                $retorno['semanal'][strtoupper(utf8_encode(strftime('%a', strtotime($key['data']))))]
-                ['ENERGIA'] = $key['dado'];
+                $retorno['semanal'][strtoupper(utf8_encode(strftime('%a', strtotime($value['data']))))]
+                ['ENERGIA'] += $value['dado'];
             }
         }
         return $retorno;
@@ -36,7 +51,7 @@ class Grafico
         foreach ($dados as $key) 
         { 
             $retorno['selecao'][date('d/m', strtotime($key['data']))]
-            ['ENERGIA'] = $key['dado'];
+            ['ENERGIA'] += $key['dado'];
         }
         return $retorno;
     }
